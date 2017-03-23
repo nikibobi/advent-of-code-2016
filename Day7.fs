155d95d7a0b09268db96208d05a213109009c1bd
bosak <bosakmaw@gmail.com>
2017-03-24 00:52:56 +0200

    day7 part1 done

---
+++ Day7.fs
@@ -0,0 +1,32 @@
+module Day7
+open System
+open System.Text.RegularExpressions
+
+let hasABBA (s: string) =
+    let isABBA =
+        function
+        | [|a;b;b';a'|] when a = a' && b = b' && a <> b -> true
+        | _ -> false
+    (Seq.windowed 4 s)
+    |> Seq.exists (isABBA)
+
+let solve (s: string) =
+    let outside, inside = 
+        s.Split('[', ']')
+        |> Array.mapi (fun i s -> i, s)
+        |> Array.partition (fun (i, _) -> i % 2 = 0)
+    outside
+    |> Array.map (snd)
+    |> Array.exists (hasABBA)
+    &&
+    inside
+    |> Array.map (snd)
+    |> Array.forall (hasABBA >> not)
+
+let day7 (input: seq<string>) =
+    input
+    |> Seq.map (solve)
+    |> Seq.filter (id)
+    |> Seq.length
+    |> (printfn "%A")
+    ()
--- Program.fs
+++ Program.fs
@@ -7,14 +7,16 @@ open Day3
 open Day4
 open Day5
 open Day6
+open Day7
 
 [<EntryPoint>]
 let main argv =
     printfn "Advent of Code!"
-    File.ReadLines("inputs/day1.txt") |> day1
-    File.ReadLines("inputs/day2.txt") |> day2
-    File.ReadLines("inputs/day3.txt") |> day3
-    File.ReadLines("inputs/day4.txt") |> day4
-    File.ReadLines("inputs/day5.txt") |> day5
-    File.ReadLines("inputs/day6.txt") |> day6
+    // File.ReadLines("inputs/day1.txt") |> day1
+    // File.ReadLines("inputs/day2.txt") |> day2
+    // File.ReadLines("inputs/day3.txt") |> day3
+    // File.ReadLines("inputs/day4.txt") |> day4
+    // File.ReadLines("inputs/day5.txt") |> day5
+    // File.ReadLines("inputs/day6.txt") |> day6
+    File.ReadLines("inputs/day7.txt") |> day7
     0
